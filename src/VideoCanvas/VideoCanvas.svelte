2ff365a40ba3186d01b1fc3764fc3d3446228264
“smlc” <siesa.marlon@hotmail.fr>
2021-02-12 20:24:44 +0000

    working on video controller

--- src/VideoCanvas/VideoCanvas.svelte
+++ src/VideoCanvas/VideoCanvas.svelte
@@ -64,7 +64,12 @@
             <canvas id="canvas" width="480" height="270"></canvas>
         </div>
         <div class="col-span-2 bg-green-400"> 
-            333
+            <div class="flex">
+                <button class="inline-flex items-center">
+                    <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                </button>
+                
+            </div>
         </div>
 </div>
 
