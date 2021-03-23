73aa3a140644c8638e2190f0c15d5acd3339878c
smlc <siesa.marlon@hotmail.fr>
2021-03-23 17:01:49 +0000

    add icon and play button

--- src/VideoCanvas/VideoCanvas.svelte
+++ src/VideoCanvas/VideoCanvas.svelte
@@ -42,13 +42,29 @@
         
 	});
 
-    function videoLoop() {
+    const videoLoop = () => {
         if (videoNode && !videoNode.paused && !videoNode.ended) {
             canvasContext.drawImage(videoNode, 0, 0, 480, 270);
         }
         setTimeout(videoLoop, 1000 / 30);
     }
 
+    const playVideo = () => {
+        if (video.paused || video.ended) {
+            video.play();
+            
+        } else {
+            video.pause();
+        }
+
+        const playbackIcons = document.querySelectorAll('.playback-icons use');
+        playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+    }
+
+    const updateProgressBar = (event) => {
+        let pos = (e.pageX  - (this.offsetLeft + this.offsetParent.offsetLeft)) / this.offsetWidth;
+        video.currentTime = pos * video.duration;
+    }
 </script>
 
 <video class="hidden" id="video" controls width="480" height="270" >
@@ -60,18 +76,38 @@
     <div class="row-span-3 bg-red-400">
         2
     </div>
-        <div class="row-span-2 col-span-2  bg-blue-500 m-auto">
-            <canvas id="canvas" width="480" height="270"></canvas>
-        </div>
-        <div class="col-span-2 bg-green-400"> 
-            <div class="flex">
-                <button class="inline-flex items-center">
-                    <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                </button>
-                
+    <div class="row-span-2 col-span-2  bg-blue-500 m-auto">
+        <canvas id="canvas" width="480" height="270"></canvas>
+    </div>
+    <div class="col-span-2 w-full bg-green-400"> 
+        <div id="video-controller w-full" class="flex">
+            <button on:click={playVideo} class="inline-flex items-center border-0">
+                <svg class="playback-icons w-10 h-10" fill="none" stroke="currentColor" >
+                    <use id="playIconSvg" href="#play-icon"></use>
+                    <use id="pauseIconSvg" class="hidden" href="#pause-icon"></use>
+                  </svg>
+            </button>
+            <div class="video-progress w-full relative mt-2">
+                <progress on:click={updateProgressBar} class="absolute appearance-none pointer-events-none w-10/12 bg-gray-500 top-0 h-4 border-0 rounded " id="progress-bar" value="0" min="0"></progress>
             </div>
         </div>
+    </div>
 </div>
 
+
+<svg style="display: none">
+
+    <defs>
+        <symbol id="play-icon" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </symbol>
+    </defs>
+
+    <defs>
+        <symbol id="pause-icon" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>        </symbol>
+    </defs>
+
+</svg>
 
 
